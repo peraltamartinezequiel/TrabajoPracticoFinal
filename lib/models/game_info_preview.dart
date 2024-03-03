81c47eca074ef98d564ec4280753c10bb9ab1b98
Tatiana Gutiérrez <90115998+tatianagutierrez@users.noreply.github.com>
2024-03-03 20:05:53 -0300

    conecto con la api. uso datos en home y game_list

--- lib/models/game_info_preview.dart
+++ lib/models/game_info_preview.dart
@@ -2,5 +2,16 @@ class GameInfoPreview {
   String name;
   String cover;
 
-  GameInfoPreview(this.name, this.cover);
+  GameInfoPreview({required this.name, required this.cover});
+
+    factory GameInfoPreview.fromJson(Map<String, dynamic> json) {
+    String cover = json.containsKey('cover') && json['cover'] != null
+        ? 'https:${json['cover']['url']}'
+        : 'https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk=';
+
+    return GameInfoPreview(
+      name: json['name'] as String,
+      cover: cover,
+    );
+  }
 }
